a025d352b37a2a93a2b259b3eca1029827daded5
Md Eiasin <ariyaneiasin02@gmail.com>
2024-10-02 22:42:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+function App() {
+  
+  return (
+  <>
+    <h2 className='bg-red-400 text-white text-center'>Hello Bangladesh!</h2>
+  </>
+  )
+}
+
+export default App
